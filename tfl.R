997d57275a3cbd02104d16ffd7176f01e59647a5
tommylee0340 <tommylee0340@gmail.com>
2017-11-11 21:35:06 +0000

    Updated 11/11

--- tfl.R
+++ tfl.R
@@ -7,6 +7,9 @@ rsconnect::setAccountInfo(name='zhuohao',
 
 library(jsonlite)
 library(httr)
+library(lubridate)
+library(dplyr)
+library(shiny)
 
 # #setting up the GET request url varibales
 # url1 = 'https://api.tfl.gov.uk/Line/'
@@ -22,26 +25,37 @@ library(httr)
 # json_data = fromJSON(text_data)
 
 #Define a function to convert seconds to min secs
-minSec = function(x){
-  if(x<60){
-    min_Sec = paste(x,'seconds',sep=' ')
-  }else if (x %% 60 == 0) {
-    min_Sec = paste(x/60, 'minutes',sep=' ')
-  }else
-    min_Sec = paste(floor(x/60),'mins',x %% 60, 'sec', sep = ' ')
-  return(min_Sec)
+minSec <-  function(x){
+  
+  ifelse(x<60,paste(x,'seconds',sep=' '),
+         ifelse(x %% 60 == 0,paste(x/60, 'minutes',sep=' '),
+                paste(floor(x/60),'mins',x %% 60, 'sec', sep = ' ')
+                )
+         )
 }
 
-library(dplyr)
+#Define a function to return the last character of a string
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+
+#Define a function to return the decision based on a letter
+boundDirect <- function(x){
+  ifelse(x == 'E','Eastbound',
+         ifelse(x == 'W','Westbound',
+                ifelse(x == 'N', 'Northbound',
+                       'Southbound')))
+}
 
 #creating the line, station name lookup table
+
+StationNodesDescription <- read.csv("StationNodesDescription.csv")
+
 Line_Station = StationNodesDescription %>%
   select(Line,Station,NAPTAN) %>%
   filter(Line != '') %>%
   distinct() %>%
   arrange(Line)
-
-library(shiny)
 
 #Define UI
 
@@ -66,7 +80,8 @@ ui = fluidPage(
                     'Waterloo & City'
                   ),
                   selected = 'Piccadilly'),
-      uiOutput('stop')
+      uiOutput('stop'),
+      uiOutput('direction')
     ),
     mainPanel(
       tableOutput('tubeArrival')
@@ -75,32 +90,68 @@ ui = fluidPage(
 )
 
 #Define server logic
-server = function(input,output){
+server <-  function(input,output){
   
-  output$stop = renderUI({
+  output$stop <-  renderUI({
     stationNames = filter(Line_Station, Line == input$line) %>%
       select(Station)
     selectInput('stop', label = 'Select a Station', stationNames, selected = 'South Kensington')
   })
   
-  output$tubeArrival = renderTable({
-
+  #the interactive direction select box
+  output$direction = renderUI({
+    
     #setting up the GET request url varibales
-    url1 = 'https://api.tfl.gov.uk/Line/'
-    line = input$line
-    stopPointId = as.character(
-                                unique(Line_Station$NAPTAN[Line_Station$Station == input$stop])
-                              )
-    url2 = '/Arrivals?stopPointId='
-    url3 = '&app_id=4b54b268&app_key=cecb6d43290dda6a86edea437a15b536'
-    url = paste(url1,line,url2,stopPointId,url3,sep = '')
-
+    url1 <-  'https://api.tfl.gov.uk/Line/'
+    line <-  input$line
+    stopPointId <-  as.character(
+      unique(Line_Station$NAPTAN[Line_Station$Station == input$stop])
+    )
+    url2 <-  '/Arrivals?stopPointId='
+    url3 <-  '&app_id=4b54b268&app_key=cecb6d43290dda6a86edea437a15b536'
+    url <-  paste(url1,line,url2,stopPointId,url3,sep = '')
+    
     #GET request and convert JSON to a dataframe
     data = GET(url)
     text_data = content(data,as = 'text')
     json_data = fromJSON(text_data)
-    final_data = json_data[c('platformName','towards','currentLocation','timeToStation')]
+    json_data$bound <-  substr(as.character(json_data$platformName),1,1)
+    selectInput('direction', label = 'Select a Direction', boundDirect(unique(json_data$bound)))
   })
+  
+  output$tubeArrival <-  renderTable({
+
+    #setting up the GET request url varibales
+    url1 <-  'https://api.tfl.gov.uk/Line/'
+    line <-  input$line
+    stopPointId <-  as.character(
+                                unique(Line_Station$NAPTAN[Line_Station$Station == input$stop])
+                              )
+    url2 <-  '/Arrivals?stopPointId='
+    url3 <-  '&app_id=4b54b268&app_key=cecb6d43290dda6a86edea437a15b536'
+    url <-  paste(url1,line,url2,stopPointId,url3,sep = '')
+
+    #GET request and convert JSON to a dataframe
+    data <-  GET(url)
+    text_data <-  content(data,as = 'text')
+    json_data <-  fromJSON(text_data)
+    json_data$timeToArrive = minSec(json_data$timeToStation)
+    json_data <- json_data[order(json_data$timeToStation),]
+    json_data$bound <-  substr(as.character(json_data$platformName),1,1)
+    json_data$platform <-  substrRight(as.character(json_data$platformName),1)
+    cleaned_data <- subset(json_data, boundDirect(json_data$bound) == input$direction)
+    # final_data <-  cleaned_data[,c('platform','towards','timeToArrive','currentLocation')]
+    final_data <-  data.frame(cleaned_data$platform,cleaned_data$towards,cleaned_data$timeToArrive,cleaned_data$currentLocation,check.names = TRUE)
+    names(final_data) <- c('Plat.','To','ETA','Current Loc.')
+     # final_data <- rename(final_data,'A' = 'cleaned_data.platform',
+     #                         'T' = 'cleaned_data.towards',
+     #                         'E' = 'cleaned_data.timeToArrive',
+     #                          'C' = 'cleaned_data.currentLocation')
+    final_data <- final_data
+  },
+  striped = TRUE
+  , hover = TRUE
+  , align = 'c')
   
 }
 
